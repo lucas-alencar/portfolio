ac2d54c99e44bb3eacf519f8cf2ba95d63ccca9d
lucas-alencar <lucasalencar.rd@gmail.com>
2022-10-19 13:04:15 -0300

    Adding typing animation

---
+++ js/typingEffect.js
@@ -0,0 +1,40 @@
+const textArray = ["javascript", "spring", "PHP", "React", "NodeJS", "MySQL", "PostgreSQL"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 100;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+
+let typedTextSpan = document.querySelector(".typed-text")
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else {
+        setTimeout(erase, newTextDelay+ 800);
+    }
+}
+
+function erase() {
+    if(charIndex > 0){
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0,charIndex-1);
+        charIndex--;
+        setTimeout(erase,erasingDelay);
+    }
+    else{
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length){
+            textArrayIndex = 0;
+        }
+        setTimeout(type,typingDelay);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    setTimeout(type, newTextDelay);
+});
+
+
